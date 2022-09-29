873e9d05f5d0c884a38fb82ae95aa122db9651d2
Yuliia <yuliiadd@gmail.com>
2022-09-29 16:18:23 +0300

    remove console.log

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ luckyBtn.addEventListener('click', changeLogo);
 
 function changeLogo() {
     const currentLogo = document.querySelector('.logo__img');
-    console.log(currentLogo.src);
     currentLogo.classList.add('change__logo');
     currentLogo.src = 'img/logo_il-makiage.png'
 }
